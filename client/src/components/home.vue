49827344e3ac9adbb25a932c62152b61b8cc91e0
Tyler Storm <stormtv@mail.uc.edu>
2018-02-05 22:21:22 -0500

    Remove some templating

---
+++ client/src/components/home.vue
@@ -0,0 +1,17 @@
+<template>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    data () {
+      return {
+        msg: ''
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
