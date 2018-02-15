7729670668e76414296bc0fc6091721cdc8baa3c
Göran Krampe <goran.krampe@evothings.com>
2018-02-15 09:27:55 +0100

    Got verified to true, try nodejs test.js

--- test.js
+++ test.js
@@ -1,55 +1,7 @@
 // Packages
-const express = require('express');
-const config = require('./config.json');
-const app = express();
-const bodyParser = require('body-parser');
 const colog = require('colog');
-const moment = require('moment');
-const models = require('./models');
 const nacl = require('tweetnacl/nacl'); //We are using a forked version of tweetnacl, so need to import nacl
 const blake = require('blakejs');
-
-// Application config
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Routes
-const alias = require('./routes/alias');
-const request = require('./routes/request');
-const address = require('./routes/address');
-
-//Debug Logging
-app.use(function (req, res, next) {
-  if (config.debug) {
-    colog.log(colog.color(moment().format('YYYY-MM-DD HH:mm:ss') + ' - ','cyan') +
-              colog.color(req.headers['x-forwarded-for'] || req.connection.remoteAddress,'cyan')+
-              ' - '+colog.inverse(req.method)+' - '+colog.bold(req.url));
-  }
-  next();
-});
-
-// Set routes
-app.use('/alias', alias);
-app.use('/address', address);
-app.use('/request', request);
-app.get('/reset', (req, res) => {
-  if (config.environment === "development") {
-    models.sequelize.sync({force:true});
-    res
-      .status(200)
-      .json({
-        'status': 'SUCCESS',
-        'message': 'Successfully cleared database'
-      });
-  } else {
-    res
-      .status(403)
-      .json({
-        'status': 'ERROR',
-        'message': 'Database reset is only available on development!'
-      });
-  }
-});
 
 // general functions
 function stringFromHex (hex) {
@@ -274,9 +226,4 @@ let message = "fuck this shit, it sucks";
 let uint8msg = hex_uint8(message);
 let signed = nacl.sign.detached(uint8msg, hex_uint8(sk));
 let verified = nacl.sign.detached.verify(uint8msg, signed, hex_uint8(kfa));
-
-models.alias.hasMany(models.code);
-models.sequelize.sync().then(function () {
-  app.listen(config.system.port);
-});
-console.log('Listening on port: ' + config.system.port);
+console.log('Verified: ' + verified)
