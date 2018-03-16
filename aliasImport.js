6efe729696f09ac2c9f28bc3b9bc09a4808313f2
Tyler Storm <stormtv@mail.uc.edu>
2018-03-15 20:50:58 -0400

    Update to new nanode system

--- aliasImport.js
+++ aliasImport.js
@@ -1,31 +1,14 @@
 const rp = require('request-promise-native');
 const Alias = require('./services/alias');
-let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 let results = [];
 let methods = {};
-let count = alphabet.length;
-let handleNanodeResponse = (aliases,letter) => {
-  if (aliases.length >= 20) {
-    count+=alphabet.length-1;
-    for (let i = 0; i < alphabet.length; i++) {
-      let newLetter = letter+alphabet[i];
-      let options = {
-        uri: "https://www.nanode.co/api/alias/lookup?id="+newLetter,
-        method: "GET",
-        json:true
-      };
-      rp(options).then((aliases) => {
-        handleNanodeResponse(aliases,newLetter);
-      }).catch((err) => {
-        console.log(err);
-      });
-    }
-  } else {
-    count--;
-    for (let n = 0; n < aliases.length; n++) {
-      results.push(aliases[n]);
-    }
-    if (count === 0) {
+methods.importAliases = () => {
+    let options = {
+      uri: "https://www.nanode.co/api/alias/all",
+      method: "GET",
+      json:true
+    };
+    rp(options).then((results) => {
       for (let i = 0; i < results.length; i++) {
         results[i].address = results[i].account;
         Alias
@@ -46,22 +29,8 @@ let handleNanodeResponse = (aliases,letter) => {
             //   });
           });
       }
-    }
-  }
-};
-methods.importAliases = () => {
-  for (let i = 0; i < alphabet.length; i++) {
-    let letter = alphabet[i];
-    let options = {
-      uri: "https://www.nanode.co/api/alias/lookup?id="+letter,
-      method: "GET",
-      json:true
-    };
-    rp(options).then((aliases) => {
-      handleNanodeResponse(aliases,letter);
     }).catch((err) => {
       console.log(err);
     });
-  }
 }
 module.exports = methods;
--- test.js
+++ test.js
@@ -2,5 +2,5 @@ const aliasSign = require('./aliasSign.js');
 let secretKey = "2AF92C6F8AC53819B97D50EA23801CDB90D98941386911AAD47B797AA3DCB9DE";
 let publicKey = "xrb_1poop3j1trsys99zw9eebmia45wtrqhjrgeswqjwd9x6i3rzsihrbta6mpwp";
 let alias = "stormtv";
-let aliasSeed = "99c97db9f9b6b141fa076ceadbe6ad4ac9311dda903b08f509cc907c030c66ea";
+// let aliasSeed = "99c97db9f9b6b141fa076ceadbe6ad4ac9311dda903b08f509cc907c030c66ea";
 console.log(aliasSign.signature([alias,publicKey],secretKey));
